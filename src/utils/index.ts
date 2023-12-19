4d928bdb4945ceb850cc25f7ecefeb54f0c4f246
史庆亮 <shiqingliang@192.168.0.100>
2023-12-19 22:52:38 +0800

    isVoid cleanObject

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,18 @@
 
-export const isFalseValue = (value: unknown) => (value === 0 ? false : !value);
+export const isFalse = (value: unknown) => (value === 0 ? false : !value);
 
+/** 是否是无效值 */
 export const isVoid = (value: unknown) => {
-  return value === undefined || value === null || value === "";
+  return value === undefined || value === null || value === "" || (Array.isArray(value) && value.length === 0);
 };
+
+/** 清理对象上的无效属性值(空值) */
+export const cleanObject = (obj: Record<string, unknown>) => {
+  const copyObj = {...obj}
+  Object.keys(copyObj).forEach((key) => {
+    if (isVoid(obj[key])) {
+      delete copyObj[key];
+    }
+  });
+  return copyObj;
+}
